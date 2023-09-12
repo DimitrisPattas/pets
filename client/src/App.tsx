e315a48e220c60324724b2e02520cedef6ce082a
DimitrisPattas <dimitris.pattas.dev@gmail.com>
2023-09-12 10:51:42 +0300

    add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button';
+import Navbar from './components/Navbar/navbar.component';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained">Hello world</Button>
+      <Navbar></Navbar>
     </div>
   );
 }
